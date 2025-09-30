3ab7d37d66ac3e0584405076823fac66d95dc147
longswin1804 <105997178@student.swin.edu.au>
2025-09-30 14:54:11 +0700

    initial setup

---
+++ controlJS/layout.js
@@ -0,0 +1,21 @@
+function loadComponent(id, file) {
+	fetch(file)
+		.then(response => {
+			if (!response.ok) {
+				throw new Error(`Could not load ${file}`);
+			}
+			return response.text();
+		})
+		.then(data => {
+			document.getElementById(id).innerHTML = data;
+		})
+		.catch(error => {
+			console.error("Error:", error);
+		});
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+	loadComponent("upper", "components/upper.html");
+	loadComponent("lower", "components/lower.html");
+});
+
